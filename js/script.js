07f22b9502c2ea471f0a8d00619344125b26aede
XiaoAaWei <xiaoawei1997@163.com>
2022-12-01 21:49:02 +0800

    Site updated: 2022-12-01 21:49:02

--- js/script.js
+++ js/script.js
@@ -1,50 +1,96 @@
-
-//导航菜单
-  layui.use(['jquery', 'element', 'carousel'], function(){
-      var $ = layui.jquery
-      ,element = layui.element
-      ,carousel = layui.carousel
-    
-      //滚动监听
-      $(window).scroll(function() {
-        var scr=$(document).scrollTop();
-        scr > 0 ? $(".nav").addClass('scroll') : $(".nav").removeClass('scroll');
-      });
-
-    //导航切换
-      var btn = $('.nav').find('.nav-list').children('button')
-      ,spa = btn.children('span')
-      ,ul = $('.nav').find('.nav-list').children('.layui-nav');
-      btn.on('click', function(){
-        if(!$(spa[0]).hasClass('spa1')){
-          spa[0].className = 'spa1';
-          spa[1].style.display = 'none';
-          spa[2].className = 'spa3';
-          $('.nav')[0].style.height = 90 + ul[0].offsetHeight + 'px';
-        }else{
-          spa[0].className = '';
-          spa[1].style.display = 'block';
-          spa[2].className = '';
-          $('.nav')[0].style.height = 60 + 'px';
+(function ($, window) {
+ 
+    var App = App || {
+        init: function () {
+            $('.js-toggle-search').on('click', function () {
+                $('.js-search').toggleClass('is-visible');
+            });
+ 
+            $('.js-next a').on('click', function (e) {
+                $(infinite_scroll.contentSelector).infinitescroll(infinite_scroll);
+ 
+                var $body = $('body');
+ 
+                $body.scrollTop($body.scrollTop() - 1);
+ 
+                e.preventDefault();
+            })
+ 
+            $(window).keydown(function (event) {
+                if (event.keyCode == 27) {
+                    if ($('.js-search').attr('class').indexOf('is-visible') > 0) {
+                        $('.js-search').removeClass('is-visible');
+                    }
+                }
+            });
+ 
+            $('.js-search .text-input').keydown(function (event) {
+                if (event.keyCode == 13) {
+                    location.href = 'https://www.google.com/search?q=site:yumemor.com ' + $(this).val();
+                    return false;
+                }
+            })
         }
-      });
-  });
+    };
+ 
 
-//返回顶部
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("gotop").style.display = "block";
-    } else {
-        document.getElementById("gotop").style.display = "none";
-    }
-}
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+    $(App.init);
 
+    $(function(){
+ 
+        var navToggle = $('#nav-toggle'),
+            nav = $('nav'),
+            navLinks = nav.find('a');
+ 
+        navToggle.on('click', function () {
+            navToggle.toggleClass('active');
+            nav.toggleClass('open');
+            return false;
+        });
+        navLinks.on('click', function () {
+            navToggle.toggleClass('active');
+            nav.toggleClass('open');
+        });
+ 
+        $(document).on('click', function () {
+            if (nav.hasClass('open')) {
+                navToggle.toggleClass('active');
+                nav.toggleClass('open');
+            }
+        });
+ 
+        $('.btn-slide').click(function () {
+            $('#panel').slideToggle("slow");
+            $(this).toggleClass("active");
+            return false;
+        });
+ 
+        $(window).scroll(function () {
+            var header = $('header');
+ 
+            if ($(this).scrollTop() > 1) {
+                header.addClass("scrolled");
+            } else {
+                header.removeClass("scrolled");
+            }
+        });
+ 
+        $("#social-share").click(function () {
+            $("#social").toggleClass("visible").slideToggle(200);
+        });
+ 
+        if ($('.welcome')[0]) {
+            $('.author-info').hide();
+            $('span.info-edit').click(function () {
+                $('.author-info').toggle();
+            });
+        }
 
+        var bannerNode = $('.top-image');
+        if(bannerNode.data('enable')){
+            var index = parseInt((Math.random() * 4) + 1);
+            bannerNode.attr('style','background-image:url(/banner/'+index+'.jpg)');
+        }
+    })
 
-
-
+}(jQuery, window));
